9cb575efb442a6853205f383d60e719dacaff609
munjeongmunjeong72-hub <munjeongmunjeong72@gmail.com>
2025-12-24 17:31:10 +0900

    Add files via upload

--- css function/js/script2.js
+++ css function/js/script2.js
@@ -1,5 +1,7 @@
 $(function(){
 
+    $(".wrap").css({"margin-bottom":50})
+
     $("li").css({"list-style":"none"})
 $("ul").css({"width":"870px","height":"202px","display":"flex","gap":"20px"})
 
@@ -11,5 +13,27 @@ $("ul li.b3").css({"width":"202px","height":"202px","background":"yellow","borde
 
 $("ul li.b4").css({"width":"202px","height":"202px","background":"green","border":"1px solid #000"})
 
+// 숨김버튼을 누르면 숨겨지는 이벤트
+// class>중복사용가능 / id>중복사용 불가능
+$("#btn1").click(function(){
+$("ul li:first-child").hide()
+})
 
+// 보이는 이벤트
+$("#btn2").click(function(){
+    $("ul li:first-child").show()
+})
+// 보였다 숨기는 이벤트
+$("#btn3").click(function(){
+    $("ul li:nth-child(3)").toggle()
+})
+// 100*100 되는 이벤트
+$("#btn4").click(function(){
+    $("ul li:last-child").width(100)
+    $("ul li:last-child").height(100)
+})
+$("#btn5").click(function(){
+    $("ul li:last-child").width(202)
+    $("ul li:last-child").height(202)
+})
 })
