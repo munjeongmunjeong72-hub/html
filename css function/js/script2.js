e7db26b9a885d6912a232808e9c022600f3bb84b
munjeongmunjeong72-hub <munjeongmunjeong72@gmail.com>
2025-12-24 16:50:28 +0900

    Add files via upload

---
+++ css function/js/script2.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    $("li").css({"list-style":"none"})
+$("ul").css({"width":"870px","height":"202px","display":"flex","gap":"20px"})
+
+$("ul li.b1").css({"width":"202px","height":"202px","background":"red","border":"1px solid #000"})
+
+$("ul li.b2").css({"width":"202px","height":"202px","background":"orange","border":"1px solid #000"})
+
+$("ul li.b3").css({"width":"202px","height":"202px","background":"yellow","border":"1px solid #000"})
+
+$("ul li.b4").css({"width":"202px","height":"202px","background":"green","border":"1px solid #000"})
+
+
+})
