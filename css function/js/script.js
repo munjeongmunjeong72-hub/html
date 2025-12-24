82503d2008b325ede985a7a819a0dce607a4b3ff
munjeongmunjeong72-hub <munjeongmunjeong72@gmail.com>
2025-12-24 16:26:52 +0900

    Add files via upload

---
+++ css function/js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+
+$("h1").css({"color":"red"})
+$(".date").css({"color":"#fff","background":"purple","font-size":80})
+$(".para").css({"border":"10px solid blue"})
+$("ul li").css({"border":"1px solid red"})
+$("ul li:last-child").css({"font-size":50})
+$("a").css({"border":"10px solid blue","padding":"15px 10px"})
+
+})
