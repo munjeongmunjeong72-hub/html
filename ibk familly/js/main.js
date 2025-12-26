ee22a8938c3b293cfb4b2c8635b936427a8dd319
munjeongmunjeong72-hub <munjeongmunjeong72@gmail.com>
2025-12-26 17:29:40 +0900

    Add files via upload

--- ibk familly/js/main.js
+++ ibk familly/js/main.js
@@ -4,9 +4,12 @@ $(function(){
     $(".family .familylist").hide()
 
      $(".family2 button").click(function(){
-        $(".family2 .familylist").toggle() })
+        $(".family2 .familylist").toggle()
+    $(".family .familylist").hide()
+    })
 
     $(".family button").click(function(){
-        $(".family .familylist").toggle()  
+        $(".family .familylist").toggle()
+         $(".family2 .familylist").hide()
     })
 })
