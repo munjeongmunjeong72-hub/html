57628bca7208847efd87a4769338b0e63c8cb78c
munjeongmunjeong72-hub <munjeongmunjeong72@gmail.com>
2025-12-29 12:40:55 +0900

    Add files via upload

---
+++ 20 asan/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.slick').slick({
+       autoplay: true,
+       dots: true,
+       autoplaySpeed:2000,
+       arrows:false,
+    //    fade:true,
+       infinite:false,
+       pauseOnHover:true,
+       slidesToScroll:1
+    })
+})
