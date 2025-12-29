e932ac242d02a8cf44d31939d72bf530f173f094
munjeongmunjeong72-hub <munjeongmunjeong72@gmail.com>
2025-12-29 15:40:59 +0900

    Add files via upload

---
+++ 30 kor/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.visaul').slick({
+       autoplay: true,
+       dots: true,
+       autoplaySpeed:2000,
+       arrows: false,
+    //    fade:true,
+       infinite:true,
+       pauseOnHover:true,
+       slidesToScroll:1
+    })
+})
