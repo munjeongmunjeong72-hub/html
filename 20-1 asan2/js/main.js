b28be17938d5aa10700a01067ba84594378bcac9
munjeongmunjeong72-hub <munjeongmunjeong72@gmail.com>
2025-12-29 14:26:20 +0900

    Add files via upload

---
+++ 20-1 asan2/js/main.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+autoplay:{
+   delay:3000,
+   disableOnInteraction:false,
+   pauseOnMouseEnter:true,
+},
+  // If we need pagination
+  pagination:{
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
